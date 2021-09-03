835db393b929cee63cc2f53afa606c8eaf57ed5a
RedGhoul <avaneesab5@gmail.com>
2021-09-03 08:34:24 -0400

    Did SLLCycleCheck

---
+++ Java/src/com/JavaAlgos/Jose/SLLCycleCheck.java
@@ -0,0 +1,12 @@
+package com.JavaAlgos.Jose;
+
+public class SLLCycleCheck {
+    /**
+     * The way you do this is start with two pointers.
+     * P1 = Slow
+     * P2 = Fast (move forward by two)
+     * Then you keep moving the two pointers along
+     * If P2 == null then there is no cycle
+     * If P2 == P1 then there is a cycle
+     * */
+}
