e3d330f3207ad2f23503affc6fe8e89640c39b27
RedGhoul <avaneesab5@gmail.com>
2021-09-06 17:39:36 -0400

    Did SLL Reversal

---
+++ Java/src/com/JavaAlgos/Jose/LLReversal.java
@@ -0,0 +1,20 @@
+package com.JavaAlgos.Jose;
+
+public class LLReversal {
+    /**
+     * Reverse a linked list pretty classic interview problem
+     * Draw it out if you have to
+     * they give the head
+     *
+     * cur = GHead
+     * next = GHead.Next
+     * prev = null
+     * while (next != null){
+     *     next = cur.next
+     *     cur.next = prev
+     *     prev = cur
+     *     cur = next
+     * }
+     *
+     * */
+}
