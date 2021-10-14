55dfaa6a2a151e77c4811d5cb79133b47a526384
RedGhoul <avaneesab5@gmail.com>
2021-10-14 13:25:45 -0400

    3 months leeeeeeetttt cooooooding it up

---
+++ Java/src/com/JavaAlgos/LeetCode/Top100/DiameterofBinaryTree.java
@@ -0,0 +1,124 @@
+package com.JavaAlgos.LeetCode.Top100;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DiameterofBinaryTree {
+
+    /**
+     * Given the root of a binary tree, return the length of the diameter of the tree.
+     *
+     * The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+     *
+     * The length of a path between two nodes is represented by the number of edges between them.
+     *
+     *
+     *
+     * Example 1:
+     *
+     *
+     * Input: root = [1,2,3,4,5]
+     * Output: 3
+     * Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+     * Example 2:
+     *
+     * Input: root = [1,2]
+     * Output: 1
+     *
+     *
+     * Constraints:
+     *
+     * The number of nodes in the tree is in the range [1, 104].
+     * -100 <= Node.val <= 100
+     *
+     * What I am thinking.
+     * It has to go through the root, you are just looking for the
+     * upside down "V"
+     *
+     * So find the longest path on either side and you got your answer
+     *
+     * So need to find the max depth on each side of the tree
+     *
+     * The basic Premise of the "V" was completely WRONG
+     *
+     * You need to longest parth between these nodes, it doesn't nessarly need to
+     * pass through the root node.
+     *
+     * You basically need to calculate the length from either side of the node
+     * your currently at. So it more of a side to side thing then a top down thing
+     *
+     * // this down here was also wrong
+     * Its just the longest path wins aka MAX depth
+     * So you were on the right track, but just made it more complex
+     * then it needed to be
+     * **/
+    public static void main(String[] args){
+
+    }
+
+//    public int diameterOfBinaryTree(TreeNode root) {
+//        if(root == null) return 0;
+//        int maxDia = 0;
+//
+//        List<Integer> max1 = new ArrayList<>();
+//        MaxD(root.left,0,0,max1);
+//        int maxDia1 = Collections.max(max1);
+//        if(maxDia1 > 0){
+//            maxDia1--;
+//        }
+//        List<Integer> max2 = new ArrayList<>();
+//        MaxD(root.left,0,0,max2);
+//        int maxDia2 = Collections.max(max2);
+//        if(maxDia2 > 0){
+//            maxDia2--;
+//        }
+//
+//        maxDia = 2 + maxDia1 + maxDia2;
+//
+//        return maxDia;
+//    }
+
+//    public int diameterOfBinaryTree(TreeNode root) {
+//        if(root == null) return 0;
+//
+//        List<Integer> max1 = new ArrayList<>();
+//        MaxD(root.left,0,0,max1);
+//        int maxDia1 = Collections.max(max1);
+//        return maxDia1;
+//    }
+//
+//    public static void MaxD(TreeNode node , int max, int curMax, List<Integer> bag){
+//        if(node == null) return;
+//        max++;
+//        if(max > curMax){
+//            curMax = max;
+//            bag.add(curMax);
+//        }
+//        if(node.left != null){
+//            MaxD(node.left,max,curMax, bag);
+//        }
+//        if(node.right != null){
+//            MaxD(node.right,max,curMax, bag);
+//        }
+//    }
+
+
+    int max = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return max;
+    }
+
+    private int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        max = Math.max(max, left + right);
+
+        return Math.max(left, right) + 1;
+    }
+}
---
+++ Java/src/com/JavaAlgos/LeetCode/Top100/MergeTwoBinaryTrees.java
@@ -0,0 +1,94 @@
+package com.JavaAlgos.LeetCode.Top100;
+
+import com.JavaAlgos.Stephen.Tree;
+
+public class MergeTwoBinaryTrees {
+    /**
+     * ou are given two binary trees root1 and root2.
+     *
+     * Imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not. You need to merge the two trees into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of the new tree.
+     *
+     * Return the merged tree.
+     *
+     * Note: The merging process must start from the root nodes of both trees.
+     *
+     *
+     *
+     * Example 1:
+     *
+     *
+     * Input: root1 = [1,3,2,5], root2 = [2,1,3,null,4,null,7]
+     * Output: [3,4,5,5,4,null,7]
+     * Example 2:
+     *
+     * Input: root1 = [1], root2 = [1,2]
+     * Output: [2,2]
+     *
+     *
+     * Constraints:
+     *
+     * The number of nodes in both trees is in the range [0, 2000].
+     * -104 <= Node.val <= 104
+     *
+     *
+     * What I am thinking:
+     *
+     * I would just have to traverse the tree. and add up the values /
+     * add a new node
+     *
+     * It doesn't always need to use your generic traversal algo
+     *
+     * It can be like generic recursive algo
+     * **/
+    public static void main(String[] args){
+
+    }
+
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1 == null){
+            return root2;
+        }
+
+        if(root2 == null){
+            return  root1;
+        }
+
+        root1.val = root1.val + root2.val;
+        // if they both end up being null they return a null
+        // which just ends up being what this left side ends up being
+        root1.left = mergeTrees(root1.left,root2.left);
+        root1.right = mergeTrees(root1.right,root2.right);
+        return root1;
+    }
+
+//    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+//        TreeNode root3 = new TreeNode();
+//        mergeTreeHelper(root1,root2,root3);
+//        return root3;
+//    }
+//
+//    public static void mergeTreeHelper(TreeNode root1, TreeNode root2, TreeNode root3){
+//        if(root1 == null && root2 == null){
+//            root3 = null;
+//            return;
+//        }
+//
+//        if(root3 == null){
+//            root3 = new TreeNode();
+//            root3.left = new TreeNode();
+//            root2.right = new TreeNode();
+//        }
+//
+//        if(root1 != null && root2 != null){
+//            root3.val = root1.val + root2.val;
+//        }else if(root1 == null){
+//            root3.val = root2.val;
+//            root1
+//        }else if(root2 == null) {
+//            root3.val = root1.val;
+//        }
+//
+//        mergeTreeHelper(root1.left, root2.left, root3.left);
+//        mergeTreeHelper(root1.right, root2.right, root3.right);
+//    }
+}
